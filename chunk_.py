fac813e064ffffae43a7c33f9f4bfc42af676945
sesankm <sesank.mallikarjuna@gmail.com>
2022-01-29 21:56:02 -0600

    accept cli arguments

--- chunk_.py
+++ chunk_.py
@@ -2,6 +2,7 @@ import math
 import os
 from pydub import AudioSegment
 from pydub.utils import make_chunks
+import sys
 
 # cconvert minutes to milliseconds
 min_to_mil = lambda x : x * 60 * 1000
@@ -17,4 +18,10 @@ def chunk_file(file_name: str, chunk_size: int):
         end = round(data.duration_seconds / 60,1) if ind == len(chunks) - 1 else (ind + 1) * chunk_size
         chunk.export("./{}/{}-{}.mp3".format(folder_name, ind * chunk_size, end), format='mp3')
 
-chunk_file('11-03303.mp3', 10)
+
+if __name__ == "__main__":
+    if(len(sys.argv) < 3):
+        print("ERROR Give a file name and chunk size\nUsage: python3 chunk_.py {file name} {duration}")
+        exit(-1)
+    print("Chunking file {} into {} minute segments...".format(sys.argv[1], sys.argv[2]))
+    chunk_file(sys.argv[1], int(sys.argv[2]))
